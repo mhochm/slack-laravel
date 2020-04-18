22ab99c87dd498a84c517c69c01a822532c5ce73
mhochm <MarcoRudolfHochmuth@googlemail.com>
2020-04-18 10:54:02 +0200

    Update ServiceProvider to make it Laravel 7 compatible

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -62,18 +62,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     {
         $app = $this->app;
 
-        $version = intval($app::VERSION);
-
-        switch ($version) {
-            case 4:
-              return new ServiceProviderLaravel4($app);
-
-            case 5:
-              return new ServiceProviderLaravel5($app);
-
-            default:
-              throw new RuntimeException('Your version of Laravel is not supported');
-        }
+        return new ServiceProviderLaravel5($app);
     }
 
     /**
